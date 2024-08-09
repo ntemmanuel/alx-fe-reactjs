ca243cf113f324eae56b7c88bc5aa0f1eb010bd1
Emmanuel Ntakirutimana <emmanuelntakirutimana250@gmail.com>
2024-08-09 12:32:47 +0200

    Initial commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './WelcomeMessage'
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import UserProfile from './UserProfile';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -23,11 +24,13 @@ function App() {
       <h1>Hello world</h1> */}
      
 
-      <Header />
+      {/* <Header />
 
       <MainContent />
 
-      <Footer />
+      <Footer /> */}
+
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
 
       {/* <WelcomeMessage/> */}
     </>
