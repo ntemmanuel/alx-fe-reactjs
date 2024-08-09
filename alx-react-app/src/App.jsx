5c7553134a398b297ed65734c0cfa07bf1a3f362
Emmanuel Ntakirutimana <emmanuelntakirutimana250@gmail.com>
2024-08-09 10:08:40 +0200

    Second commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import WelcomeMessage from './WelcomeMessage'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -18,7 +18,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>Hello world</h1>
+      <WelcomeMessage/>
     </>
+    
   )
 }
 
