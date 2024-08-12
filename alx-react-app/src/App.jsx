958e69b3c11f07979f3e2ca6c6c3e42adc4ae13f
Emmanuel Ntakirutimana <emmanuelntakirutimana250@gmail.com>
2024-08-12 09:55:01 +0200

    Initial commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,41 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import WelcomeMessage from './WelcomeMessage'
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
-import UserProfile from './UserProfile';
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      {/* <h1>Vite + React</h1>
-      <h1>Hello world</h1> */}
-     
-
-      {/* <Header />
-
-      <MainContent />
-
-      <Footer /> */}
-
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-
-      {/* <WelcomeMessage/> */}
+      <WelcomeMessage/>
     </>
     
-  )
+  );
 }
 
-export default App
+export default App;
