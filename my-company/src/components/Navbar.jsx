b77e822822dfde4f7cbdc967d62f99ed730d9eef
Emmanuel Ntakirutimana <emmanuelntakirutimana250@gmail.com>
2024-08-16 14:23:21 +0200

    Initial commit

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,10 +1,8 @@
-// src/Navbar.jsx
-import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#eee' }}>
+    <nav style={{ display: 'flex', justifyContent: 'space-between', padding: '10px', backgroundColor: '#f8f9fa' }}>
       <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
       <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>
