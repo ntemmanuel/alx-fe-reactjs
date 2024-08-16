7781f225e06c519de6642d68c9885716612ee6b1
Emmanuel Ntakirutimana <emmanuelntakirutimana250@gmail.com>
2024-08-16 11:55:18 +0200

    Initial commit

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,9 +3,13 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import React from 'react';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
-  
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
 
   return (
     <>
@@ -13,7 +17,10 @@ function App() {
       {/* <Header />
       <MainContent />
       <Footer /> */}
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      {/* <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> */}
+      <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
     </>
     
   );
