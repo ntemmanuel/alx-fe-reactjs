2c67d12e97e86ecf655270fb758418871d78a8e5
Emmanuel Ntakirutimana <emmanuelntakirutimana250@gmail.com>
2024-08-16 10:46:48 +0200

    Initial commit

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   
@@ -9,10 +10,11 @@ function App() {
   return (
     <>
      
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      {/* <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Header />
       <MainContent />
-      <Footer />
+      <Footer /> */}
+      <Counter initialCount={0} />
     </>
     
   );
