ce17710b76a835144db17be70c5d0dba22ba97da
Emmanuel Ntakirutimana <emmanuelntakirutimana250@gmail.com>
2024-08-30 14:04:44 +0200

    Initial commit

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,26 @@
+// src/App.jsx
+import React from 'react';
+// Import the controlled form
+import RegistrationForm from './components/RegistrationForm';
+// Import the Formik form
+import FormikForm from './components/FormikForm';
+import './index.css';
+function App() {
+  return (
+    <div>
+      <h1>User Registration</h1>
+      
+      {/* Controlled Form */}
+      <h2>Controlled Form</h2>
+      <RegistrationForm />
+
+      <hr />
+
+      {/* Formik Form */}
+      <h2>Formik Form</h2>
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
