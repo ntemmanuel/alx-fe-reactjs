af19a12ef69846b3496d9d6656ff6d527bedc230
Emmanuel Ntakirutimana <emmanuelntakirutimana250@gmail.com>
2024-08-30 14:39:22 +0200

    Initial commit

--- form-handling-react/src/components/FormikForm.js
+++ form-handling-react/src/components/FormikForm.js
@@ -1,4 +1,4 @@
-// src/components/formikForm.jsx
+// src/components/formikForm.js
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
